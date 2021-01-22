69fb7d1a1708eebc9489be5d2e9d7805a764a37c
guzlej <martin.guzlej@gmail.com>
2021-01-22 16:14:55 +0000

    Routing and wiring up

--- app/router.js
+++ app/router.js
@@ -8,15 +8,15 @@ export default class Router extends EmberRouter {
 
 Router.map(function() {
   this.route('gallery', function() {
-    this.route('bane');
-    this.route('fono');
-    this.route('selec');
-    this.route('c1');
-    this.route('chill');
-    this.route('ghetto');
-    this.route('kids');
-    this.route('ludo');
-    this.route('paul');
-    this.route('raja');
+    this.route('bane', { path: '/Bottle-Bane' });
+    this.route('fono', { path: '/Bathroom-Fono' });
+    this.route('selec', { path: '/Bathroom-Selec' });
+    this.route('c1', { path: '/Bench-C1' });
+    this.route('chill', { path: '/Chillout-Zone' });
+    this.route('ghetto', { path: '/Toy-Ghetto' });
+    this.route('kids', { path: '/Kids-room' });
+    this.route('ludo', { path: '/Stool-Ludo' });
+    this.route('paul', { path: '/House-Paulen' });
+    this.route('raja', { path: '/Chair-Raja' });
   });
 });
