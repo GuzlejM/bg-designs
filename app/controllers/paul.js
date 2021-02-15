793b6de14fa216bb679e3eb21a5b611e58153f84
guzlej <martin.guzlej@gmail.com>
2021-02-15 01:41:35 +0000

    grid-dense testing

---
+++ app/controllers/paul.js
@@ -0,0 +1,51 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  items: [
+    {
+      src: 'assets/ph/1.jpg',
+      w: 768,
+      h: 768,
+    },
+    {
+      src: 'assets/ph/6.jpg',
+      w: 768,
+      h: 768,
+    },
+    {
+      src: 'assets/ph/2.jpg',
+      w: 768,
+      h: 768,
+    },
+    {
+      src: 'assets/ph/8.jpg',
+      w: 768,
+      h: 768,
+    },
+    {
+      src: 'assets/ph/5.jpg',
+      w: 768,
+      h: 768,
+    },
+    {
+      src: 'assets/ph/4.jpg',
+      w: 768,
+      h: 768,
+    },
+    {
+      src: 'assets/ph/7.jpg',
+      w: 768,
+      h: 768,
+    },
+    {
+      src: 'assets/ph/7.jpg',
+      w: 768,
+      h: 768,
+    },
+    {
+      src: 'assets/ph/3.jpg',
+      w: 768,
+      h: 768,
+    }
+  ]
+});
--- app/router.js
+++ app/router.js
@@ -7,16 +7,15 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
-  this.route('gallery', function() {
-    this.route('bane', { path: '/Bottle-Bane' });
-    this.route('fono', { path: '/Bathroom-Fono' });
-    this.route('selec', { path: '/Bathroom-Selec' });
-    this.route('c1', { path: '/Bench-C1' });
-    this.route('chill', { path: '/Chillout-Zone' });
-    this.route('ghetto', { path: '/Toy-Ghetto' });
-    this.route('kids', { path: '/Kids-room' });
-    this.route('ludo', { path: '/Stool-Ludo' });
-    this.route('paul');
-    this.route('raja', { path: '/Chair-Raja' });
-  });
+  this.route('gallery')
+  this.route('bane', { path: '/Bottle-Bane' });
+  this.route('fono', { path: '/Bathroom-Fono' });
+  this.route('selec', { path: '/Bathroom-Selec' });
+  this.route('c1', { path: '/Bench-C1' });
+  this.route('chill', { path: '/Chillout-Zone' });
+  this.route('ghetto', { path: '/Toy-Ghetto' });
+  this.route('kids', { path: '/Kids-room' });
+  this.route('ludo', { path: '/Stool-Ludo' });
+  this.route('paul');
+  this.route('raja', { path: '/Chair-Raja' });
 });
