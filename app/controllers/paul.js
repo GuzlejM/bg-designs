614d87e99f0ca4c3d19c19b0c12d2d68bceac2de
guzlej <martin.guzlej@gmail.com>
2021-02-15 11:48:50 +0000

    paul slide changed to a tag  linked template

--- app/controllers/paul.js
+++ app/controllers/paul.js
@@ -4,38 +4,38 @@ export default Ember.Controller.extend({
   items: [
     {
       src: 'assets/ph/5.jpg',
-      w: 768,
+      w: 1024,
       h: 768,
     },
     {
       src: 'assets/ph/6.jpg',
-      w: 768,
+      w: 1024,
       h: 768,
     },
     {
       src: 'assets/ph/8.jpg',
-      w: 768,
+      w: 1024,
       h: 768,
     },
     {
       src: 'assets/ph/4.jpg',
-      w: 768,
+      w: 1200,
       h: 768,
     },
     {
       src: 'assets/ph/2.jpg',
-      w: 768,
+      w: 1024,
       h: 768,
     },
     {
       src: 'assets/ph/7.jpg',
-      w: 768,
+      w: 1200,
       h: 768,
     },
     {
       src: 'assets/ph/3.jpg',
-      w: 768,
-      h: 768,
+      w: 700,
+      h: 1024,
     }
   ]
 });
