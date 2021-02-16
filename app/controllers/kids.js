8741e6824ee4b3f9cf3e54f2e65eb264dbb27928
guzlej <martin.guzlej@gmail.com>
2021-02-16 07:28:48 +0000

    kids room finished

---
+++ app/controllers/kids.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  items: [
+    {
+      src: 'assets/cr/1.jpg',
+      w: 1024,
+      h: 768,
+    },
+    {
+      src: 'assets/cr/2.jpg',
+      w: 1024,
+      h: 768,
+    },
+    {
+      src: 'assets/cr/3.jpg',
+      w: 1024,
+      h: 768,
+    },
+    {
+      src: 'assets/cr/4.jpg',
+      w: 1024,
+      h: 768,
+    },
+    {
+      src: 'assets/cr/5.jpg',
+      w: 1024,
+      h: 768,
+    }
+  ]
+});
